15869697a5ab5a72d8b155da9ad92066127f842b
Katarina <kat9234@gmail.com>
2018-11-18 15:39:26 -0800

    PIE-6: Used for loop instead of repetition to show image/title/text in help section

--- template-parts/help-section.php
+++ template-parts/help-section.php
@@ -7,52 +7,29 @@
 
   if(get_field('enable_help_section_cf')): ?>
     <div class="container text-center">
+      <?php if(get_field('help_section_title')): ?>
+          <h1 id="help-section-title"><?php the_field('help_section_title') ?></h1>
+      <?php endif ?>
+
       <section>
-        <?php if(get_field('help_section_title')): ?>
-            <h1 id="help-section-title"><?php the_field('help_section_title') ?></h1>
-        <?php endif ?>
         <div class="row">
-          <div class="col-md-4 col-xs-12">
-            <?php if(get_field('hs_left_column_image')): ?>
-              <div style="background-image: url(<?php echo get_field('hs_left_column_image')['url']; ?>)" class="help-section-image rounded-circle"></div>
-            <?php endif ?>
+          <?php
+          $positions = array('right', 'middle', 'left');
+          foreach ($positions as $position) { ?>
+            <div class="col-md-4 col-xs-12">
+              <?php if(get_field('hs_' . $position . '_column_image')): ?>
+                <div style="background-image: url(<?php echo get_field('hs_' . $position . '_column_image')['url']; ?>)" class="help-section-image rounded-circle"></div>
+              <?php endif ?>
 
-            <?php if(get_field('hs_left_column_title')): ?>
-                <h2><?php the_field('hs_left_column_title') ?></h2>
-            <?php endif ?>
+              <?php if(get_field('hs_' . $position .'_column_title')): ?>
+                  <h2><?php the_field('hs_' . $position .'_column_title') ?></h2>
+              <?php endif ?>
 
-            <?php if(get_field('hs_left_column_text')): ?>
-                <?php the_field('hs_left_column_text') ?>
-            <?php endif ?>
-          </div>
-
-          <div class="col-md-4 col-xs-12">
-            <?php if(get_field('hs_middle_column_image')): ?>
-              <div style="background-image: url(<?php echo get_field('hs_middle_column_image')['url']; ?>)" class="help-section-image rounded-circle"></div>
-            <?php endif ?>
-
-            <?php if(get_field('hs_middle_column_title')): ?>
-                <h2><?php the_field('hs_middle_column_title') ?></h2>
-            <?php endif ?>
-
-            <?php if(get_field('hs_middle_column_text')): ?>
-                <?php the_field('hs_middle_column_text') ?>
-            <?php endif ?>
-          </div>
-
-          <div class="col-md-4 col-xs-12">
-            <?php if(get_field('hs_right_column_image')): ?>
-              <div style="background-image: url(<?php echo get_field('hs_right_column_image')['url']; ?>)" class="help-section-image rounded-circle"></div>
-            <?php endif ?>
-
-            <?php if(get_field('hs_right_column_title')): ?>
-                <h2><?php the_field('hs_right_column_title') ?></h2>
-            <?php endif ?>
-
-            <?php if(get_field('hs_right_column_text')): ?>
-                <?php the_field('hs_right_column_text') ?>
-            <?php endif ?>
-          </div>
+              <?php if(get_field('hs_' . $position .'_column_text')): ?>
+                  <p><?php the_field('hs_' . $position .'_column_text') ?><p>
+              <?php endif ?>
+            </div>
+          <?php } ?>
         </div>
       </section>
     </div>
