6678577c3bce4f42643066662b7509fbcc449e6d
Katarina <kat9234@gmail.com>
2018-11-18 19:31:05 -0800

    PIE-6: Added the light grey background and the wave at the bottom using svg

--- template-parts/help-section.php
+++ template-parts/help-section.php
@@ -6,32 +6,41 @@
  */
 
   if(get_field('enable_help_section_cf')): ?>
-    <div class="container text-center">
-      <?php if(get_field('help_section_title')): ?>
-          <h1 id="help-section-title"><?php the_field('help_section_title') ?></h1>
-      <?php endif ?>
+    <div class="help-section">
+      <div class="container text-center">
+        <?php if(get_field('help_section_title')): ?>
+            <h1 id="help-section-title"><?php the_field('help_section_title') ?></h1>
+        <?php endif ?>
 
-      <section>
-        <div class="row">
-          <?php
-          $positions = array('right', 'middle', 'left');
-          foreach ($positions as $position) { ?>
-            <div class="col-md-4 col-xs-12">
-              <?php if(get_field('hs_' . $position . '_column_image')): ?>
-                <div style="background-image: url(<?php echo get_field('hs_' . $position . '_column_image')['url']; ?>)" class="help-section-image rounded-circle"></div>
-              <?php endif ?>
+        <section>
+          <div class="row">
+            <?php
+            $positions = array('right', 'middle', 'left');
+            foreach ($positions as $position) { ?>
+              <div class="col-md-4 col-xs-12">
+                <?php if(get_field('hs_' . $position . '_column_image')): ?>
+                  <div style="background-image: url(<?php echo get_field('hs_' . $position . '_column_image')['url']; ?>)" class="help-section-image rounded-circle"></div>
+                <?php endif ?>
 
-              <?php if(get_field('hs_' . $position .'_column_title')): ?>
-                  <h2><?php the_field('hs_' . $position .'_column_title') ?></h2>
-              <?php endif ?>
+                <?php if(get_field('hs_' . $position .'_column_title')): ?>
+                    <h2><?php the_field('hs_' . $position .'_column_title') ?></h2>
+                <?php endif ?>
 
-              <?php if(get_field('hs_' . $position .'_column_text')): ?>
-                  <p><?php the_field('hs_' . $position .'_column_text') ?><p>
-              <?php endif ?>
-            </div>
-          <?php } ?>
-        </div>
-      </section>
+                <?php if(get_field('hs_' . $position .'_column_text')): ?>
+                    <p><?php the_field('hs_' . $position .'_column_text') ?><p>
+                <?php endif ?>
+              </div>
+            <?php } ?>
+          </div>
+        </section>
+      </div>
     </div>
+
+    <div id="help-section-wave">
+      <svg viewBox="0 0 500 150" preserveAspectRatio="none">
+        <path d="M 0 50 Q 83 100 167 50 Q 250 0 333 50 Q 417 100 500 50 L500.00,0.00 L0.00,0.00 Z"></path>
+      </svg>
+    </div>
+
   <?php endif;
 ?>
