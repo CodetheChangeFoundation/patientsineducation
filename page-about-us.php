ab4acae7265d4da64c7b6cb88a7275360b8256a5
AliciaYang <aliciaxyang@gmail.com>
2018-11-21 18:32:41 -0800

    improve styling

--- page-about-us.php
+++ page-about-us.php
@@ -6,7 +6,7 @@
 <?php get_header(); ?>
 <div>
   <h1 class="text-center font-weight-bold text-large">More About PIE</h1>
-  <div class="container">
+  <div class="container mx-0 px-0 mw-100">
     <?php get_template_part('template-parts/more-about-pie'); ?>
   </div>
 </div>
