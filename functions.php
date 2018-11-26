ca52a9a00b6a7f47650845f7f5bdab7e58e1fa18
Katarina1234 <kat9234@gmail.com>
2018-11-25 20:48:59 -0800

    PIE-28: Created four icon section (#30)
    * PIE-28: 4-icon custom field, funcitons.php, unfinished css, unfinished template
    
    * PIE-28 four icon css, csslint fixes
    
    * PIE-28: Fixed alignment of icon and title in 4-icon section
    
    * PIE-28: Space fix in css
    
    * PIE-28: Spacing fixes in css

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/theme-support.php';
 require get_template_directory() . '/custom-fields/paragraph.php';
 require get_template_directory() . '/custom-fields/help-section.php';
+require get_template_directory() . '/custom-fields/four-icon-section.php';
 require get_template_directory() . '/custom-fields/project-description.php';
 
 add_shortcode( 'get_icon_url', function( $atts ){
