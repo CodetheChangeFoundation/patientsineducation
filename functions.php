f199b8c537a0ee84c4b90b91cc68c8c34984fde2
Katarina <kat9234@gmail.com>
2018-12-02 22:09:19 -0800

    PIE-23: Organizations we work with table

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@ require get_template_directory() . '/inc/theme-support.php';
 require get_template_directory() . '/custom-fields/paragraph.php';
 require get_template_directory() . '/custom-fields/help-section.php';
 require get_template_directory() . '/custom-fields/four-icon-section.php';
+require get_template_directory() . '/custom-fields/organizations-section.php';
 require get_template_directory() . '/custom-fields/project-description.php';
 
 function pie_scripts_enqueue() {
