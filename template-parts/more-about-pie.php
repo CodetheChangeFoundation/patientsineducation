7b524e1b0ae1cf1f1d611c3fa884f41ea19388a9
AliciaYang <aliciaxyang@gmail.com>
2018-12-18 13:52:32 -0800

    add circles

--- template-parts/more-about-pie.php
+++ template-parts/more-about-pie.php
@@ -12,6 +12,7 @@ $more_about_us_4 = get_field('more_about_us_4');
     <div class="row row-odd position-relative">
         <div class="thumbnail-1 col-12 col-md-6 col-lg-6 order-1" style="background-image: url(<?php echo $more_about_us_1['thumbnail']; ?>);"></div>
         <div class="content-container-1 col-12 col-md-6 col-lg-6 order-2">
+          <span class="dot-1"></span>
           <h3 class="font-weight-bold title-1"><?php echo $more_about_us_1['title']; ?></h1>
           <p class="content-text-1"><?php echo $more_about_us_1['content']; ?></p>
         </div>
@@ -21,6 +22,7 @@ $more_about_us_4 = get_field('more_about_us_4');
     <div class="row row-even position-relative">
         <div class="thumbnail-2 col-12 col-md-6 col-lg-6 order-3 order-md-4 order-lg-4" style="background-image: url(<?php echo $more_about_us_2['thumbnail']; ?>);"></div>
         <div class="content-container-2 text-right col-12 col-md-6 col-lg-6 order-4 order-md-3 order-lg-3">
+          <span class="dot-2"></span>
           <h3 class="font-weight-bold title-2"><?php echo $more_about_us_2['title']; ?></h1>
           <p class="content-text-2"><?php echo $more_about_us_2['content']; ?></p>
         </div>
@@ -30,6 +32,7 @@ $more_about_us_4 = get_field('more_about_us_4');
     <div class="row row-odd position-relative">
         <div class="thumbnail-3 col-12 col-md-6 col-lg-6 order-5" style="background-image: url(<?php echo $more_about_us_3['thumbnail']; ?>);"></div>
         <div class="content-container-3 col-12 col-md-6 col-lg-6 order-6">
+          <span class="dot-3"></span>
           <h3 class="font-weight-bold title-3"><?php echo $more_about_us_3['title']; ?></h1>
           <p class="content-text-3"><?php echo $more_about_us_3['content']; ?></p>
         </div>
@@ -39,6 +42,7 @@ $more_about_us_4 = get_field('more_about_us_4');
     <div class="row row-even position-relative">
         <div class="thumbnail-4 col-12 col-md-6 col-lg-6 order-7 order-md-8 order-lg-8" style="background-image: url(<?php echo $more_about_us_4['thumbnail']; ?>);"></div>
         <div class="content-container-4 text-right col-12 col-md-6 col-lg-6 order-8 order-md-7 order-lg-7">
+          <span class="dot-4"></span>
           <h3 class="font-weight-bold title-4"><?php echo $more_about_us_4['title']; ?></h1>
           <p class="content-text-4"><?php echo $more_about_us_4['content']; ?></p>
         </div>
