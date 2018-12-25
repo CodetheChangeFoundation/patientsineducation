b16c6383528d31f0a23c4c870aa5cbfc5929f12a
Kass (Kasidit) Ouitayakul <37598987+kouitayakul@users.noreply.github.com>
2018-12-24 18:36:07 -0800

    PIE-1: Created navigation bar (#16)
    * first commit
    
    * combining Bootstrap with WordPress
    
    * need review for navbar
    
    * Fixed indenting

--- functions.php
+++ functions.php
@@ -9,6 +9,7 @@ require get_template_directory() . '/inc/cleanup.php';
 require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/theme-support.php';
 require get_template_directory() . '/custom-fields/paragraph.php';
+require get_template_directory() . '/inc/bs4navwalker.php';
 require get_template_directory() . '/custom-fields/help-section.php';
 require get_template_directory() . '/custom-fields/four-icon-section.php';
 require get_template_directory() . '/custom-fields/organizations-section.php';
