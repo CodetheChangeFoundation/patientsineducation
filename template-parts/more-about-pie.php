549f8e4fc4412bdf0c9eb3158f8778187502894d
Pritpal Chauhan <pritpal@Pritpals-MacBook-Pro.local>
2019-01-27 02:13:14 -0800

    PIE-47: Refactored more about patients section to flex

--- template-parts/more-about-pie.php
+++ template-parts/more-about-pie.php
@@ -23,11 +23,11 @@ if (get_field('enable_more_about_pie')):
       ?>
         <div class="d-flex flex-column flex-md-row align-items-center row-<?php echo $row; ?>">
           <div
-            class="mb-4 thumbnail d-flex flex-fill about-pie-img order-<?php echo 2*$index-1; ?> <?php if ($row == "even"){echo "order-md-".(string)(2*$index);}?> <?php if ($row == "even"){echo "order-lg-".(string)(2*$index);}?>"
+            class="mb-5 thumbnail d-flex flex-fill about-pie-img order-<?php echo 2*$index-1; ?> <?php if ($row == "even"){echo "order-md-".(string)(2*$index);}?> <?php if ($row == "even"){echo "order-lg-".(string)(2*$index);}?>"
             style="background-image: url('<?php echo the_sub_field('image'); ?>');"
           >
           </div>
-          <div class="mb-4 mx-3 d-flex flex-fill flex-column p-4 col-<?php echo $row; ?> order-<?php echo 2*$index; ?> <?php if ($row == "even"){echo "order-md-".(string)(2*$index-1);}?> <?php if ($row == "even"){echo "order-lg-".(string)(2*$index-1);}?>">
+          <div class="mb-5 mx-5 d-flex flex-fill flex-column p-4 col-<?php echo $row; ?> order-<?php echo 2*$index; ?> <?php if ($row == "even"){echo "order-md-".(string)(2*$index-1);}?> <?php if ($row == "even"){echo "order-lg-".(string)(2*$index-1);}?>">
             <h3 class="font-weight-bold"><?php echo the_sub_field('title'); ?></h3>
             <p><?php echo the_sub_field('content'); ?></p>
           </div>
