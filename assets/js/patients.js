35479a553c23a9ae2720c1bdc8f7dce450c0214e
Viniel Kumar <vinielk@icloud.com>
2019-02-02 12:12:59 -0800

    PIE-45: made counter animation 2 seconds long

--- assets/js/patients.js
+++ assets/js/patients.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
             $(this).prop('Counter', 0).animate({
                Counter: $(this).text()
             }, {
-               duration: 1000,
+               duration: 2000,
                easing: 'linear',
                step: function (now) {
                   $(this).text(Math.ceil(now));
