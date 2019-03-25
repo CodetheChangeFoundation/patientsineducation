9d7f7bd9b947f7b406bc3977b71b70772dec7028
Pritpal Chauhan <30644663+pritpalc@users.noreply.github.com>
2019-03-25 00:46:13 -0700

    PIE-70: Added archived section to outreach (#72)
    * PIE-70: Added archived section to outreach
    
    * PIE-70: Added archived section to outreach

--- assets/js/patients.js
+++ assets/js/patients.js
@@ -11,6 +11,7 @@ $('.close-icon').on('click', function () {
    $('.collapse.show').collapse('hide');
 });
 
+// Homepage
 // counter animation
 var runsOnce = true;
 
@@ -44,3 +45,28 @@ $(document).ready(function () {
       }
    }).trigger('scroll');
 });
+
+// Outreach section
+var outreach = $('#outreach-archive');
+
+outreach.on('click', function() {
+   var archived = "Show Archived";
+   var classes = "col-12 col-lg-4 pt-4";
+   if (outreach[0].innerHTML == archived) {
+      changeClass(classes);
+      outreach[0].innerHTML = "Hide Archived";
+   } else {
+      changeClass(`${classes} d-none`);
+      outreach[0].innerHTML = archived;
+   }
+});
+
+function changeClass($classes) {
+   var i = 3;
+   var outreachElement = $(`#outreach-${i}`);
+   while (outreachElement[0] !== undefined) {
+      outreachElement[0].classList.value = $classes;
+      i++;
+      outreachElement = $(`#outreach-${i}`);
+   }
+}
