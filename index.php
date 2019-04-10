dd5020b051a5046ae19536aa4e1fe33300e911c8
Viniel Kumar <vinielk@icloud.com>
2019-04-10 00:42:58 -0700

    PIE-59: fix wave under navbar on chroma and safari

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (have_posts()):
   while (have_posts()): the_post();
     /* Display the page title and under-navbar styling only if it's not the home page */
     if (!is_front_page()): ?>
-      <div class="pie-under-navbar text-dark mb-4">
+      <div class="pie-under-navbar text-dark mb-4 pb-1 border-white border-bottom">
         <div class="container py-4">
           <div class="row">
             <div class="col-12 text-center px-5">
@@ -22,7 +22,9 @@ if (have_posts()):
             </div>
           </div>
         </div>
-        <div class="wave py-3 py-md-4 my-lg-5"></div>
+        <div class="wave">
+          <img class="wave-img" src="/wp-content/themes/patientsineducation/assets/svg/wave.svg" />
+        </div>
       </div>
     <?php endif; ?>
   <?php endwhile; ?>
