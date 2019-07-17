3d797a4b478d655ccf79112d61d10e24ae078562
Viniel Kumar <vinielk@icloud.com>
2019-07-17 14:45:36 -0700

    PIE-61: add if check for 404 page

--- 404.php
+++ 404.php
@@ -14,9 +14,11 @@ $link = get_field('404_page_button', 'option');
   <div class="row">
     <div class="col-12">
       <?php the_field('404_page_content', 'option'); ?>
-      <div class="w-100 text-center mt-4">
-        <a class="btn btn-outline-primary btn-lg" target="<?php echo $link['target']; ?>" href="<?php echo $link['url']; ?>"><?php echo $link['title'] ?></a>
-      </div>
+      <?php if ($link): ?>
+        <div class="w-100 text-center mt-4">
+          <a class="btn btn-outline-primary btn-lg" target="<?php echo $link['target']; ?>" href="<?php echo $link['url']; ?>"><?php echo $link['title'] ?></a>
+        </div>
+      <?php endif; ?>
     </div>
   </div>
 </div>
